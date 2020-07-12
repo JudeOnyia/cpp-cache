eeccb742236da012c83bb0bfe99353917ddf4ea4
Jude Onyia <judeonyia10@gmail.com>
2020-07-12 15:43:58 -0700

    1) Used static variables for original size of matrix 2) Fix address overflow for case where matrix and result are different 3) Made sure 2D matrix is all contiguous in memory

--- app/test_matrix_transpose.cpp
+++ app/test_matrix_transpose.cpp
@@ -1,5 +1,6 @@
 #include "ra/matrix_transpose.hpp"
 #include<iostream>
+#include<cstddef>
 
 int main(){
 	using std::cin;
@@ -9,44 +10,46 @@ int main(){
 
 	std::size_t rows;
 	std::size_t cols;
-	rows = 10;//cin>>rows;
-	cols = 10;//cin>>cols;
+	rows = 6;//cin>>rows;
+	cols = 5;//cin>>cols;
 
-	type_t** matrx = new type_t*[rows];
-	for(std::size_t i=0; i<rows; ++i){
-		matrx[i] = new type_t[cols];
-	}
+	// Creating matrix[rows][cols]
+	type_t* matrx = new type_t[rows*cols];
 
-	type_t** result = new type_t*[cols];
-	for(std::size_t i=0; i<cols; ++i){
-		result[i] = new type_t[rows];
-	}
+	// Creating result[cols][rows]
+	type_t* result = new type_t[cols*rows];
 
+	// Initialize matrix and result
 	for(std::size_t i=0; i<rows; ++i){
 		for(std::size_t j=0; j<cols; ++j){
-			matrx[i][j] = i*j+1;//cin>>matrx[i][j];
+			matrx[i*cols+j] = i*cols+j;//cin>>matrx[i][j];
+			result[j*rows+i] = 0;
 		}
 	}
 
-	const type_t* a = &(matrx[0][0]);
-	type_t* b = &(result[0][0]);
-
-	ra::cache::matrix_transpose<type_t>(a,rows,cols,b);
+	// Transpose matrix
+	ra::cache::matrix_transpose<type_t>(matrx,rows,cols,result);
 	
+	// Print matrix
 	for(std::size_t i=0; i<rows; ++i){
 		for(std::size_t j=0; j<cols; ++j){
-			cout<<matrx[i][j]<<" ";
+			cout<<matrx[i*cols+j]<<" ";
 		}
 		cout<<endl;
 	}
 	cout<<endl<<endl;
 
+	// Print result
 	for(std::size_t i=0; i<cols; ++i){
 		for(std::size_t j=0; j<rows; ++j){
-			cout<<result[i][j]<<" ";
+			cout<<result[i*rows+j]<<" ";
 		}
 		cout<<endl;
 	}
+
+	// Delete matrix and result
+	delete[] matrx;
+	delete[] result;
 
 
 
--- include/ra/matrix_transpose.hpp
+++ include/ra/matrix_transpose.hpp
@@ -15,25 +15,32 @@ namespace ra::cache {
 
 	template <class T>
 	void matrix_transpose(const T* a, std::size_t m, std::size_t n, T* b){
+
+		// Preserve the values of the original number of rows and columns
 		static int initialized_original_lenghts = 0;
-		static std::size_t orgin_m;
-		static std::size_t orgin_n;
+		static std::size_t M;
+		static std::size_t N;
 		if(!initialized_original_lenghts){
-			orgin_m = m;
-			orgin_n = n;
+			M = m;
+			N = n;
 			initialized_original_lenghts = 1;
 		}
 
+		// If matrix a and b are the same, create a buffer matrix to store computation
 		bool flag_a_is_b = false;
-		T* old_b = b;
+		//T* old_b = b;
 		if(b == a){ // Handle a and b being the same
 			b = new T[m*n];
 			flag_a_is_b = true;
 		}
 
 		// Base case
-		if((m*n)<=64){
-			naive_matrix_transpose(a, m, n, b);
+		if((m*n)<=2){
+			for(std::size_t i=0; i<m; ++i){
+				for(std::size_t j=0; j<n; ++j){
+					*(b+(j*M+i)) = *(a+(i*N+j));
+				}
+			}
 		}
 		// Recurse
 		else{
@@ -41,29 +48,32 @@ namespace ra::cache {
 				std::size_t m1 = m / 2; // Number of rows in A1
 				std::size_t m2 = m - m1; // Number of rows in A2
 				const T* a1 = a; // pointer to first element in A1
-				const T* a2 = a + (m1*n); // pointer to first element in A2
+				const T* a2 = a + (m1*N); // pointer to first element in A2
 				T* b1 = b; // pointer to first element in B1
 				T* b2 = b + m1; // pointer to first element in B2
 				matrix_transpose(a1,m1,n,b1);
 				matrix_transpose(a2,m2,n,b2);
 			}
-			else{
+			else{ // Divide n
 				std::size_t n1 = n / 2; // Number of columns in A1
 				std::size_t n2 = n - n1; // Number of columns in A2
 				const T* a1 = a; // pointer to the first element in A1
 				const T* a2 = a + n1; // pointer to first element in A2
 				T* b1 = b; // pointer to first element in B1
-				T* b2 = b + (n1*m); // pointer to first element in B2
+				T* b2 = b + (n1*M); // pointer to first element in B2
 				matrix_transpose(a1,m,n1,b1);
 				matrix_transpose(a2,m,n2,b2);
 			}
 		}
 		
-		if(flag_a_is_b){ // If a and old b were same, copy b into a and free b	
-			for(std::size_t i=0; i<(m*n); ++i){
-				old_b[i] = b[i];
-			}
-			delete[] b; 
+		// If matrix a and b were same and we created buffer matrix, copy back to a and free buffer
+		if(flag_a_is_b){ // If a and old b were same, copy b into pointer pointing to same as a	
+			/*for(std::size_t i=0; i<(m*n); ++i){
+				*(old_b+i) = *(b+i);
+			}*/
+			//delete[] b;
+			delete[] a;
+			a = b;	
 		}
 	}
 
